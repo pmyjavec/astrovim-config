f58d5b3963964206e145d67b8298c1d95ea33551
Paul Myjavec <pauly@myjavec.com>
2024-03-29 20:28:19 +0900

    feat: add neotest

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,23 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = true },
-  { -- further customize the options set by the community
-    "catppuccin",
-    opts = {
-      integrations = {
-        sandwich = false,
-        noice = true,
-        mini = true,
-        leap = true,
-        markdown = true,
-        neotest = true,
-        cmp = true,
-        overseer = true,
-        lsp_trouble = true,
-        rainbow_delimiters = true,
-      },
-    },
-  },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
     "zbirenbaum/copilot.lua",
@@ -44,4 +27,5 @@ return {
   },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.test.neotest" },
 }
